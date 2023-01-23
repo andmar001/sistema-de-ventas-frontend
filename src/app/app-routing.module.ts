6aa9bd2e4564f0bfb64a42ccb2d7d7f6618dd5ab
tony tolver <andrademaresantonio00@gmail.com>
2023-01-22 22:46:34 -0600

    Ruteo Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:LoginComponent, pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component:LoginComponent, pathMatch: 'full'
+  },
+  {
+    path: 'pages',
+    loadChildren: () => import('./Components/layout/layout.module').then(m => m.LayoutModule)  // Lazy loading
+  },
+  {
+    path: '**', // !Wildcard route for a 404 page
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
